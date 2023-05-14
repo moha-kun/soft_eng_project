fd9dd5e9b0268ed66b71483d22e0fe31215bece1
Abdelwahed AB <ababdelwahed112@gmail.com>
2023-05-14 17:55:49 +0100

    Add endpoints to get available drivers and vehicles

--- src/main/java/org/moha/miniproject/rest/ConducteurController.java
+++ src/main/java/org/moha/miniproject/rest/ConducteurController.java
@@ -1,31 +1,34 @@
 package org.moha.miniproject.rest;
 
-import org.moha.miniproject.Repositories.UserRepository;
 import org.moha.miniproject.enteties.Conducteur;
 import org.moha.miniproject.services.conducteur.ConducteurService;
-import org.moha.miniproject.services.verification.UserVerification;
+import org.moha.miniproject.services.disponibilite.DisponibiliteService;
 import org.moha.miniproject.dto.PasswordUpdateDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 public class ConducteurController {
-
-    @Autowired
-    private UserVerification userVerification;
-
     @Autowired
     private ConducteurService conducteurService;
 
     @Autowired
-    private UserRepository userRepository;
+    private DisponibiliteService disponibiliteService;
 
     @GetMapping("/conducteurs")
     public List<Conducteur> getConducteurs() {
         return conducteurService.getAllDrivers();
+    }
+
+    @GetMapping("/conducteurs/disponible")
+    public List<Conducteur> getDisponibleConducteurs(
+            @RequestParam(name = "dateDepart") LocalDate dateDepart,
+            @RequestParam(name = "dateArrive")LocalDate dateArrive){
+        return disponibiliteService.getAvailableConducteurs(dateDepart, dateArrive);
     }
 
     @PostMapping("/conducteurs")
@@ -54,5 +57,4 @@ public class ConducteurController {
     public void deleteConducteur(@PathVariable Long idCond){
         conducteurService.removeDriver(idCond);
     }
-
 }
